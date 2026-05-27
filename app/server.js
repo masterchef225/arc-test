cbce476db7f2c8cd6dc52877d1e6e0148c425948
Kirill Zvyagin <masterchef225@gmail.com>
2026-05-27 12:47:31 +0300

    test app build

---
+++ app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pino = require('pino');
+
+const log = pino();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.json({ ok: true, host: require('os').hostname() });
+});
+
+app.get('/health', (_req, res) => res.send('ok'));
+
+app.listen(port, () => log.info(`listening on ${port}`));
